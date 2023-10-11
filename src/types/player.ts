cd6ff082d32b4b88f95eda34b78058790ef71dd3
quochuy2405 <huy01682312920@gmail.com>
2023-10-11 16:51:19 +0700

    update name

--- src/types/player.ts
+++ src/types/player.ts
@@ -2,23 +2,23 @@ import { MapType } from "./map";
 import { MessageType } from "./message";
 
 export interface PlayerType {
-  id: string;
-  name: string;
-  direction?: string;
-  avatar?: string;
-  x?: number;
-  state?: number;
-  y?: number;
+	id: string;
+	name: string;
+	direction?: string;
+	avatar?: string;
+	x?: number;
+	state?: number;
+	y?: number;
 }
 
 export interface GamePlayContextProps {
-  playerId: string | null;
-  setCurrentPlayer: React.Dispatch<React.SetStateAction<object | PlayerType>>;
-  messages: Record<string, MessageType>;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  playerRef: any; // Type accordingly to your Firebase database reference
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  players: any;
-  currentRoom: MapType;
-  currentPlayer: PlayerType;
+	playerId: string | null;
+	setCurrentPlayer: React.Dispatch<React.SetStateAction<PlayerType>>;
+	messages: Record<string, MessageType>;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	playerRef: any; // Type accordingly to your Firebase database reference
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	players: any;
+	currentRoom: MapType;
+	currentPlayer: PlayerType;
 }
