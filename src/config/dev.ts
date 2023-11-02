636f659541f9d3c16782fd801c8fbc048fbcd2c1
Minh Khuy <haokhuy@gmail.com>
2023-11-02 14:17:45 +0700

    change token

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -6,7 +6,7 @@ export const devConfig = {
   name: `Chrome`,
   password: "pass",
   signature:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoiVzhDVnFsT0JUcG1aZjBJYkpCSFByZyIsImlhdCI6MTY5ODkwMTAwMSwiZXhwIjoxNjk4OTA4MjAxLCJ0cGMiOiJ0ZXN0IiwicHdkIjoicGFzcyIsInVzZXJfaWRlbnRpdHkiOiJDaHJvbWUiLCJyb2xlX3R5cGUiOjF9.OxiUAVXyAYk1TbBYhqi1gF9x0ZzR7lF1z7p3kYBj6yk",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoiVzhDVnFsT0JUcG1aZjBJYkpCSFByZyIsImlhdCI6MTY5ODkwOTQzOSwiZXhwIjoxNjk4OTE2NjM5LCJ0cGMiOiJ0ZXN0IiwicHdkIjoicGFzcyIsInVzZXJfaWRlbnRpdHkiOiJDaHJvbWUiLCJyb2xlX3R5cGUiOjF9.0rO7bJ8qSECM0Z1UyxZizLLgmOGzlgG7c22bj5jTZ4s",
   sessionKey: "",
   userIdentity: "",
   // role = 1 to join as host, 0 to join as attendee. The first user must join as host to start the session
